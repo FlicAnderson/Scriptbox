119bd7957a8b1510d14214f3adf11ad5d7833fed
FlicAnderson <contact@felicityanderson.co.uk>
2014-07-04 12:58:37 +0100

    finished; function; strips collection number and suffix off and leaves prefix only

---
+++ function_prefixKeeper.R
@@ -0,0 +1,28 @@
+### FUNCTION: prefixKeeper ###
+### 3rd July 2014
+### Strip away collection numbers and suffixes from collector number prefixes
+###
+
+# test values
+a <- c("M.8564", "8272", "M 8539", "M.8056A", "11448A", "11504", "M 10019", "M10382", "8381A","9851b", "44")
+
+
+# remove prefix & collNum
+#gsub("([0-9]*)*(([A-Za-z]$)?)", "", a)
+
+### function:
+prefixKeeper <- function(x){
+  gsub("([0-9]*)*(([A-Za-z]$)?)", "", x)
+}
+
+print("...function 'prefixKeeper' loaded; to use, call: prefixKeeper(x)")
+print("...strips away collection numbers and suffixes from collector numbers")
+
+
+#test:
+#prefixKeeper(a)
+
+###
+### to call: prefixKeeper(x)
+###
+###
