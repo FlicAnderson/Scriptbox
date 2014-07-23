b662200855f4f4c88ba3322ccacf40a3187e90db
FlicAnderson <contact@felicityanderson.co.uk>
2014-07-23 13:33:18 +0100

    finished; important; template format for function scripts

---
+++ function_format.R
@@ -0,0 +1,15 @@
+## FUNCTION :: function_format.R
+# ======================================================== 
+# (Day Month YYYY)
+# Author: Flic Anderson
+#
+# to call: [function]()
+# objects created: [object1]; [object2] (locally global)
+# saved at: O://CMEP\-Projects/Scriptbox/[filename]
+#
+# FUNCTION AIM: What this script is all about, why it was made, what
+# it does, etc.
+#
+# --------------------------------------------------------
+
+# CODE # 
