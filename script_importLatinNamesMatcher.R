ebd59f806c699f791f02b015dc41fb9eb69959f0
FlicAnderson <contact@felicityanderson.co.uk>
2014-08-08 14:34:48 +0100

    started to change this to be universal, but then changed it back to run import-padme names only. Changes which stuck include using file.choose to select the import-Padme location and some conditional functions which run if it's definitely a .mdb file

--- script_importLatinNamesMatcher.R
+++ script_importLatinNamesMatcher.R
@@ -41,57 +41,70 @@ source("Z:/fufluns/scripts/function_livePadmeArabiaCon.R")
 # QUESTION: 
 # NAME SOURCE TO BE MATCH-TESTED IS:
 # (A) {import padme} or similar database? (answered)
-# or
-# (B) spreadsheet?
 
 
-# 1) get names from source into dataframe ([0UPS].[nameNoAuth] -> origName) & ([0UPS].[currntDetNoAuth] -> crrntDet)
 
-# currently irrelevant #
-#source("Z:/fufluns/databasin/userPrompt_IF.R")
-#if(interactive())fun()
-# currently irrelevant #
+# source:
+importSource <- file.choose()
+# get the extension
+extns <- paste0(".", unlist(strsplit(importSource, "[.]"))[2])
+# IF extns = database: 
+#   A) extns = .mdb
+# IF extns = spreadsheet:
+#   B) extns = .xls/.xlsx
+# IF extns = comma separated value file:
+#   C) extns = .csv
+
+# A)
+dbImport <- grepl(".mdb", extns)
+
+ifelse(dbImport <- grepl(".mdb", extns), "importfile is a databse file (.mdb)", "importfile is NOT a database file (.mdb), use script_latinNamesMatcher.R instead!")
+
+
+
+# 1) get names from source into dataframe 
 
 # For source (A) - {import padme} or similar database -
 # follow these instructions... 
+# ([0UPS].[nameNoAuth] -> origName) & ([0UPS].[currntDetNoAuth] -> crrntDet)
 
-importPadmeCon()
-livePadmeArabiaCon()
-
-
-# CREATE ORIGINAL ALLPLANTS TABLE
-# DO ONCE (already done)
-# copy [0UPS] table to allow us to delete non-plants from [0UPS] but still have a copy of them somewhere ready to import if necessary. 
-qry <- "SELECT * INTO 0ALLPlants FROM 0UPS"
-sqlQuery(con_importPadme, qry)
-
-# CREATE NON-PLANTS ONLY TABLE - can be imported separately if required at a later date
-# DO ONCE
-# copy [0UPS] table to allow us to delete non-plants from [0UPS] but still have a copy of them somewhere ready to import if necessary. 
-qry <- "SELECT * INTO 0NonPlants FROM 0UPS"
-sqlQuery(con_importPadme, qry)
-
-# 0UPS will have all non-plants removed.
-
+### FUNCTION: import-copy database name import method: dbMethod
+dbMethod <- function(){
+# call functions to open connections with import padme and live padme
+  importPadmeCon()
+  livePadmeArabiaCon()
+# deal with non-plants issue, where lichens and things complicate matters:  
+  # create original ALLPlants table
+    # DO ONCE (already done)
+    # copy [0UPS] table to allow us to delete non-plants from [0UPS] but still have a copy of them somewhere ready to import if necessary. 
+    qry <- "SELECT * INTO 0ALLPlants FROM 0UPS"
+    sqlQuery(con_importPadme, qry)
+  # CREATE NON-PLANTS ONLY TABLE - can be imported separately if required at a later date, will contain non-import 'non-plants' e.g lichens
+    # DO ONCE
+    # copy [0UPS] table to allow us to delete non-plants from [0UPS] but still have a copy of them somewhere ready to import if necessary. 
+    qry <- "SELECT * INTO 0NonPlants FROM 0UPS"
+    sqlQuery(con_importPadme, qry)
+  # 0UPS will have all non-plants removed.
 
 # pull out the names from the 0UPS imported table
-#sqlColumns(con_importPadme, "0UPS")$COLUMN_NAME
-# want [nameNoAuth] and also to check through [currntDetNoAuth]
-# make objects, pull unique names into them via SQL
-# original names & id
-qry <- "SELECT id, nameNoAuth FROM [0UPS]"
-origName <- sqlQuery(con_importPadme, qry)
-# current dets & id
-qry <- "SELECT id, currntDetNoAuth FROM [0UPS]"
-crrntDet <- sqlQuery(con_importPadme, qry)
+    #sqlColumns(con_importPadme, "0UPS")$COLUMN_NAME  
+    # want [nameNoAuth] and also to check through [currntDetNoAuth]
+    # make objects, pull unique names into them via SQL
+    # original names & id
+    qry <- "SELECT id, nameNoAuth FROM [0UPS]"
+    origName <- sqlQuery(con_importPadme, qry)
+    # current dets & id
+    qry <- "SELECT id, currntDetNoAuth FROM [0UPS]"
+    crrntDet <- sqlQuery(con_importPadme, qry)
+  # CLOSE THE CONNECTION!
+  #odbcCloseAll()
+}
 
-# CLOSE THE CONNECTION!
-#odbcCloseAll()
 
-
-# For source (B) - spreadsheet -
-# follow these instructions... 
-# (UNFINISHED)
+if(dbImport==TRUE){
+  print("...using database method")
+  dbMethod()
+}
 
 
 # 2) get list of names from Padme? ([Latin Names].[sortName] -> nameZ)
