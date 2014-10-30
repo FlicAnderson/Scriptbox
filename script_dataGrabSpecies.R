7dee35ffb376029843858601603f88f152138551
FlicAnderson <contact@felicityanderson.co.uk>
2014-10-30 14:46:21 +0000

    altered script to improve comments for users, tidy up script, and also print head(data) instead of all data to console

--- script_dataGrabSpecies.R
+++ script_dataGrabSpecies.R
@@ -3,14 +3,17 @@
 # (30th October 2014)
 # Author: Flic Anderson
 #
-# dependant on: "O://CMEP\ Projects/Scriptbox/function_livePadmeArabiaCon.R"
+# dependent on: "O://CMEP\ Projects/Scriptbox/function_livePadmeArabiaCon.R"
 # saved at: O://CMEP\ Projects/Scriptbox/script_dataGrabSpeciesSocotra.R
 #
-# AIM: Pull out records into R for species in Arabia (not only Socotra) from Padme Arabia using SQL given a taxonName, print to console.  Then save as .csv for future use, in order of status in [Herbarium Specimens].[Herbaria], then [Herbarium Specimens].[FlicFound] fields.
-#
-# --------------------------------------------------------
+# AIM: Pull out records into R for species in Arabia (not only Socotra) from 
+# .... Padme Arabia using SQL given a taxonName, print to console.  
+# .... Then save as CSV file (.csv) for future use. Records ordered by fields
+# .... [Herbarium Specimens].[Herbaria], then [Herbarium Specimens].[FlicFound]
 
-# CODE # 
+# ---------------------------------------------------------------------------- #
+
+# CODE SUMMARY # 
 
 # 0) Load libraries, functions, source scripts
 # 1) Input species name
@@ -19,6 +22,7 @@
 # 4) Show the output
 # 5) Save the output to .csv
 
+# ---------------------------------------------------------------------------- #
 
 # 0) 
 
@@ -28,7 +32,6 @@ if (!require(RODBC)){
   install.packages("RODBC")
   library(RODBC)
 } 
-
 # open connection to live padme
 source("O://CMEP\ Projects/Scriptbox/function_livePadmeArabiaCon.R")
 livePadmeArabiaCon()
@@ -36,13 +39,14 @@ livePadmeArabiaCon()
 
 # 1) 
 
-### USER INPUT REQUIRED ###
-# example: 
-#taxonName <- "Cyperaceae"
-#taxonName <- "Coelocarpum"
-#taxonName <- "Dracaena cinnabari"
+###---------------------- USER INPUT REQUIRED HERE --------------------------###
+# please input the taxon you're searching for, as shown in the examples below:
+# examples: 
+  #taxonName <- "Cyperaceae"
+  #taxonName <- "Coelocarpum"
+  #taxonName <- "Dracaena cinnabari"
 taxonName <- "Coelocarpum"
-### USER INPUT REQUIRED ###
+###---------------------- USER INPUT REQUIRED HERE --------------------------###
 
 # 2)
 
@@ -62,40 +66,38 @@ Herb.FlicStatus,
 Herb.FlicIssue,
 Herb.FlicNotes
 FROM (((((([Herbarium Specimens] AS [Herb] LEFT JOIN [Geography] AS [Geog] ON Herb.Locality=Geog.ID) 
-LEFT JOIN [Herbaria] ON Herb.Herbarium=Herbaria.id) 
-LEFT JOIN [determinations] AS [Dets] ON Herb.id=Dets.[specimen key]) 
-LEFT JOIN [Synonyms tree] AS [Snym] ON Dets.[latin name key] = Snym.member) 
-LEFT JOIN [Latin Names] AS [Lnam] ON Snym.[member of] = Lnam.id) 
-LEFT JOIN [Teams] AS [Team] ON Herb.[Collector Key]=Team.id) 
-LEFT JOIN [Teams] AS [DetTeam] ON Dets.[Det by] = DetTeam.id 
+  LEFT JOIN [Herbaria] ON Herb.Herbarium=Herbaria.id) 
+    LEFT JOIN [determinations] AS [Dets] ON Herb.id=Dets.[specimen key]) 
+      LEFT JOIN [Synonyms tree] AS [Snym] ON Dets.[latin name key] = Snym.member) 
+        LEFT JOIN [Latin Names] AS [Lnam] ON Snym.[member of] = Lnam.id) 
+          LEFT JOIN [Teams] AS [Team] ON Herb.[Collector Key]=Team.id) 
+            LEFT JOIN [Teams] AS [DetTeam] ON Dets.[Det by] = DetTeam.id 
 WHERE Dets.Current=True AND Lnam.sortName LIKE '%", taxonName, "%'
 ORDER BY Herbaria.Acronym, Herb.FlicStatus, Team.[name for display], Herb.[Collection number] & '' & Herb.postfix;")
-# test the query - keep commented unless testing
-#sqlQuery(con, qry)
 
 # 3)
 
 # run query
 recGrab <- sqlQuery(con_livePadmeArabia, qry)
-
-# show number of records returned
-nrow(recGrab)
+  # show number of records returned
+  nrow(recGrab)
 
 # 4)
 
-# sort so found/unmounted float to the top when displaying in console
-recGrab[order(recGrab$FlicStatus, decreasing=TRUE, na.last=TRUE),]
+# show first 6 records returned 
+  # sorted so Edinburgh specimens, then found specimens float to the top 
+head(recGrab[order(order(recGrab$institute, recGrab$FlicFound, decreasing=TRUE, na.last=TRUE),])
 
 # 5)
 
-# reminds user to select where to save file & what to call it (need to add ".csv" on end/create file)
-message("... use opened window to choose where to save the CSV file")
-message("... enter filename including '.csv', & if asked whether to create file, say 'YES' unless saving over existing file")
-
-# write to .csv file
+### USER REMINDER: 
+# write.csv() function will ask where to save file and what to call it
+# enter filename including '.csv', & if asked whether to create file, say 'YES' 
+  # write to .csv file
+  # sorted so Edinburgh specimens, then found specimens, displayed ascendingly
 write.csv(recGrab[order(recGrab$institute, recGrab$FlicFound, na.last=TRUE),], file=file.choose())
 
 # VERY IMPORTANT!
-# CLOSE THE CONNECTION!
+# CLOSE DATABASE CONNECTIONs & REMOVE OBJECTS FROM WORKSPACE!
 odbcCloseAll()
 rm(list=ls())
