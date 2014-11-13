07737418ed51f429082472112e81f7b827d1b75e
FlicAnderson <contact@felicityanderson.co.uk>
2014-11-13 15:28:51 +0000

    useful bits and pieces including collection number splitting functions

---
+++ general_utilities/function_collNumKeeper.R
@@ -0,0 +1,37 @@
+### FUNCTION: collNumKeeper ###
+### 3rd July 2014
+### Strip away prefixes and suffixes from collector numbers
+###
+
+# test values
+a <- c("M.8564", "8272", "M 8539", "M.8056A", "11448A", "11504", "M 10019", "M10382", "8381A","9851b", "44")
+
+
+# complex way:
+# remove prefix
+#gsub("^([A-Za-z]([ ])?|[A-Za-z][\\.]([ ])?)", "", a)
+# remove suffix
+#gsub("([A-Za-z])$", "", a)
+# all together:
+# as.numeric(gsub("^([A-Za-z]([ ])?|[A-Za-z][\\.]([ ])?)", "", gsub("([A-Za-z])$", "", x)))
+
+# remove everything except collNum
+### function:
+
+collNumKeeper <- function(x){
+       gsub("[^0-9]", "", x)
+}
+
+print(". ")
+print(".. function 'collNumKeeper' loaded; to use, call: collNumKeeper(x)")
+print("... collNumKeeper() strips away prefixes and suffixes from collector numbers")
+print(".... ")
+
+#test:
+#collNumKeeper(a)
+
+###
+### to call: collNumKeeper(x)
+###
+###
+
---
+++ general_utilities/function_suffixKeeper.R
@@ -0,0 +1,27 @@
+### FUNCTION: suffixKeeper ###
+### 3rd July 2014
+### Strip away collection numbers and prefixes from collector number suffixes
+###
+
+# test values
+a <- c("M.8564", "8272", "M 8539", "M.8056A", "11448A", "11504", "M 10019", "M10382", "8381A","9851b", "44")
+
+
+### function:
+
+suffixKeeper <- function(x){
+  gsub("^([A-Za-z]([ ])?|^[A-Za-z][\\.]([ ])?)?([0-9]*)", "", x)
+}
+
+print(". ")
+print(".. function 'suffixKeeper' loaded; to use, call: suffixKeeper(x)")
+print("... suffixKeeper() strips away prefixes and collection numbers from collector numbers")
+print(".... ")
+
+#test:
+#suffixKeeper(a)
+
+###
+### to call: suffixKeeper(x)
+###
+###
