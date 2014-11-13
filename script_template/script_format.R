b218f7a66430dc88976f4b3806d75c76757f22d6
FlicAnderson <contact@felicityanderson.co.uk>
2014-11-13 15:27:49 +0000

    format template file for scripts

---
+++ script_template/script_format.R
@@ -0,0 +1,15 @@
+## [Project] :: script_format.R
+# ======================================================== 
+# (Day Month YYYY)
+# Author: Flic Anderson
+#
+# standalone script // dependant on: [filename]
+# saved at: O://CMEP\-Projects/Scriptbox/[filename]
+#
+# SCRIPT AIM: What this script is all about, why it was made, what
+# it does, etc.
+#
+# --------------------------------------------------------
+
+# CODE # 
+
