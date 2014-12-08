020a1a7091cb559cd689ec34ea11bbecae8d96f4
FlicAnderson <contact@felicityanderson.co.uk>
2014-12-08 13:21:52 +0000

    fixed user-input subset info -> numeric problem, works fine now, also improved prompts

--- database_importing/function_importNames_xlsx.R
+++ database_importing/function_importNames_xlsx.R
@@ -13,41 +13,55 @@ importNames_xlsx <- function(){
   
   # ask user whether taxon names have authorities attached 
   authCheck <<- readline(
-    prompt="... Enter 'TRUE' if taxon names HAVE authorities 
-    attached (ie. in same column), or 'FALSE' if there is NO authority 
-    information attached... "
+    prompt="........ enter 'TRUE' if taxon names HAVE authorities 
+        attached (ie. in same column), or 'FALSE' if there is NO authority 
+        information attached
+        ... "
   )
   # convert the entered text to logical
   authCheck <- as.logical(authCheck)
   
   # IF taxon names HAVE authorities attached, use [Full name] field from database
   if(sum(authCheck)==1){
-    nameVar <- "[Full name]"
+    nameVar <<- "[Full name]"
   }
   # IF taxon names DO NOT HAVE authorities attached, use [sortName] Padme field
   if(sum(authCheck)!=1){
-    nameVar <- "[sortName]"
+    nameVar <<- "[sortName]"
   }
   
   # for a subset of columns or rows, enter the indexes required:
   # REQUIRE INPUT FOR COLUMN/ROW SUBSET
-  rowIndex <- readline(prompt="... Enter row index to read in - format '1:5' ... ")
-  colIndex <- readline(prompt="... Enter column index to read in - format 'c(1,2)' - 1st column species names, 2nd column for any subspecific epithets... ")
-  #rowIndex <- 4:921
-  #colIndex <- c(4,6)  
+  rowIndex <- readline(prompt="........ enter ROW index to read in - format '1:5'
+         (if uncertain as to exact number, overestimate!) 
+         ... "
+  )
+  rowIndex <- as.numeric(unlist(strsplit(rowIndex, split=":")))[1]:as.numeric(unlist(strsplit(rowIndex, split=":")))[2]      
+  #as.numeric(rowIndex)
+  colIndex <- readline(prompt="........ enter COLUMN index to read in - format '1,2' 
+         - 1st column species names, 2nd column for any subspecific epithets 
+         (example: column 1 contains 'Adenium obesum', column 2 contains 'subsp. sokotranum')
+         ... "
+  )
+  colIndex <- c(as.numeric(unlist(strsplit(colIndex, split=",")))[1] , as.numeric(unlist(strsplit(colIndex, split=",")))[2])
+  #rowIndex <- 1:86
+  #colIndex <- c(6,7)
   
   # import the file
   #check it pulls out the right data: 
   crrntDet <<- read.xlsx(file=importSource, sheetIndex=1, 
                          colIndex=colIndex, rowIndex=rowIndex, 
                          header=TRUE)
-  
+    
   # change variable names
   # change the column names using <<- operator to allow the changes to be
   # accessible from outside the function
   names(crrntDet)
   names(crrntDet)[1] <<- "Species_Name"
   names(crrntDet)[2] <<- "Subspecific_Epithet"
+  # if it's playing up and giving "object 'crrntDet' not found, use this:
+  #names(crrntDet)[1] <- "Species_Name"
+  #names(crrntDet)[2] <- "Subspecific_Epithet"
   names(crrntDet)
   
   # missing values
@@ -59,7 +73,7 @@ importNames_xlsx <- function(){
   # find rows where is.NA for column 2 is TRUE
   crrntDet[which(is.na(crrntDet[,2])==TRUE),]
   # set these cells to empty string
-  crrntDet[which(is.na(crrntDet[,2])==TRUE),2] <<- ""
+  #crrntDet[which(is.na(crrntDet[,2])==TRUE),2] <<- ""
   
   # case problems
   # ensure subspecific epithets are all lowercase
@@ -77,8 +91,19 @@ importNames_xlsx <- function(){
   #exampl4 <- paste(exampl2[,1], exampl2[,2])
   #[1] "Peperomia tetraphylla_"
   
+  # take off any extra spaces
+  #crrntDet[,1] <- gsub("[ ]$", "", crrntDet[,1])
+  #crrntDet[,2] <- gsub("[ ]$", "", crrntDet[,2])
+  # take off any spaces at beginning
+  #crrntDet[,1] <- gsub("^[ ]", "", crrntDet[,1])
+  #crrntDet[,2] <- gsub("^[ ]", "", crrntDet[,2])
+  
   # recreating the 'full' subspecific names:
   fullnames <- paste(crrntDet[,1], crrntDet[,2])
+  #using gsub/etc to remove the NAs:
+  # pattern which finds " NA"
+  # pattern_NA <- " NA"
+  fullnames <- gsub(" NA", "", fullnames)
   #using gsub/etc to remove the additional spaces:
   # pattern which finds end spaces:
   # pattern_endspace <- "[ ]$"
@@ -96,13 +121,16 @@ importNames_xlsx <- function(){
     sum(grepl("^[A-Z][a-z]( [a-z])?", fullnames))==length(fullnames)
   }
   #run function:
-  # I currently have no idea why this seems to repeat the prints twice...
+  #nameForm <- nameFormat() 
+
+  # I currently have no idea why following seems to repeat the prints twice...
   # but it seems harmless...
-  nameForm <- nameFormat() 
   
   # return whether all names are in correct format or not
   ifelse( # condition
-    nameForm == TRUE, 
+    nameFormat() == TRUE, 
+        # I currently have no idea why following seems to repeat the prints twice...
+        # but it seems harmless...
     # do if true:
     print("... all names in correct format; carry on with analysis"), 
     # do if false:
@@ -112,7 +140,7 @@ importNames_xlsx <- function(){
   ## those with formatting issues.  It's probably best to do this by hand
   
   crrntDet$Taxon <<- fullnames
-  dim(crrntDet)
+  #dim(crrntDet)
 }
 
 # CALL & RUN function
