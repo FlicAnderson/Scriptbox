abf25092425c801b2911da2fa750e5d9ba05281c
FlicAnderson <contact@felicityanderson.co.uk>
2015-08-10 13:41:39 +0100

    template for script to pull ethnographic annotation data out for Socotran specimens for Friends of Socotra conference 2015 and subsequent analyses

---
+++ database_export/script_exportPadmeEthnographicData.R
@@ -0,0 +1,29 @@
+## Socotra Project :: script_exportPadmeEthnographicData.R
+# ============================================================================ #
+# 10 August 2015
+# Author: Flic Anderson
+#
+# standalone script // dependant on: [filename]
+# saved at: O://CMEP\-Projects/Scriptbox/database_export/script_exportPadmeEthnographicData.R
+# source: source("O://CMEP\-Projects/Scriptbox/database_export/script_exportPadmeEthnographicData.R")
+#
+# AIM:  Export ethnographic annotation data for Socotran specimens
+# ....  (ethnographic annotation data was added via ???)
+# ....  (removal and export of data based on: 
+# ....  fufluns/databasin/ethnographic/script_ethnogAnnotationQuery_Socotra-used.R)
+#
+# ---------------------------------------------------------------------------- #
+
+# CODE SUMMARY # 
+
+# 0) 
+# 1)  
+# 2) 
+# 3) 
+# 4) 
+# 5) 
+
+# ---------------------------------------------------------------------------- #
+
+
+# 0)
