91d695afbb71bc4a210c84d4ae083f696a3fe10c
FlicAnderson <contact@felicityanderson.co.uk>
2015-08-14 15:10:24 +0100

    tested joining via sqldf with tester of recGrab data & annotations & created a tester_output with the ethnog data & names, but need to redo with full dataset. BEWARE DATA SIZE ISSUES THAT WILL OCCUR WITH FULL DATASET - 159922 OBS OF 34 VARS

--- database_export/script_exportPadmeEthnographicData.R
+++ database_export/script_exportPadmeEthnographicData.R
@@ -70,25 +70,28 @@ Anlm <- sqlQuery(con_livePadmeArabia, query="SELECT * FROM [AnnotationListMember
 
 # used pull-out section of this (below) script, not whole thing as that includes summary
 # stats & writeouts
-#source("O://CMEP\ Projects/Scriptbox/database_output/script_dataGrabFullLatLonOrGazLatLon_Socotra.R")
-# get all recs as recGrab (17760 obs. of 27 variables)
+source("O://CMEP\ Projects/Scriptbox/database_output/script_dataGrabFullLatLonOrGazLatLon_Socotra.R")
+# get all recs as recGrab (17760 obs. of 29 variables)
 
-### pull out annotations for these records' taxa.
+# remove unrequired record-groups F, L & H
+rm(fielRex, litrRex, herbRex)
+
+### pull out annotations for all records' taxa.
 # SQLDF this for now after pulling out ALL annotations data into R...
 
 # things that are used?
-qry <- "SELECT 
-        Anse.id, 
-        Anti.title, 
-        Anlm.display, 
-        Lnam.sortName, 
-        Lnam.id AS LnamID 
-FROM ((Anls INNER JOIN Anlm ON Anls.selectionId = Anlm.id) INNER JOIN (Anti 
-        INNER JOIN Anse ON Anti.annotationSetId = Anse.id) ON Anlm.annotationTitleId = Anti.id) 
-        LEFT JOIN Lnam ON Anls.latinNameId = Lnam.id 
-WHERE (((Anse.id)=7) AND ((Anti.title)='Use?'));"
+# qry <- "SELECT 
+#         Anse.id, 
+#         Anti.title, 
+#         Anlm.display, 
+#         Lnam.sortName, 
+#         Lnam.id AS LnamID 
+# FROM ((Anls INNER JOIN Anlm ON Anls.selectionId = Anlm.id) INNER JOIN (Anti 
+#         INNER JOIN Anse ON Anti.annotationSetId = Anse.id) ON Anlm.annotationTitleId = Anti.id) 
+#         LEFT JOIN Lnam ON Anls.latinNameId = Lnam.id 
+# WHERE (((Anse.id)=7) AND ((Anti.title)='Use?'));"
 
-str(sqldf(qry))
+#str(sqldf(qry))
 # 'data.frame':        706 obs. of  5 variables:
 #         $ Anse.id      : int  7 7 7 7 7 7 7 7 7 7 ...
 # $ Anti.title   : chr  "Use?" "Use?" "Use?" "Use?" ...
@@ -97,7 +100,7 @@ str(sqldf(qry))
 # $ LnamID       : int  2596 2597 2598 2599 2600 2601 2602 2603 2604 2607 ...
 
 
-# ALL things: (overwrites qry)
+# ALL things: 
 qry <- "SELECT 
         Anse.id, 
         Anti.title, 
@@ -118,8 +121,11 @@ str(sqldf(qry))
 # $ LnamID       : int  2596 2597 2598 2599 2600 2601 2602 2603 2604 2607 ...
 
 # what's included?
-a <- sqldf(qry)
-table(a$Anti.title)
+        # NOTE: If this doesn't work, have you commented out the Annotation tables 
+        # queries at the top? If so, the sqldf query won't work since it's based on those!
+datA_ethnog <- sqldf(qry)
+# 
+table(datA_ethnog$Anti.title)
 # Animal Food- Specific Livestock    Animal/ Livestock Management   Commercial Value 
 # 1816                                 167                             97 
 # Construction                       Fishing                        Food (Animal) 
@@ -132,16 +138,76 @@ table(a$Anti.title)
 # Use? 
 # 706 
 
-# pull out particular category (e.g. Fishing
-a[which(a$Anti.title=="Fishing"),]
+# EXAMPLE: pull out particular category (e.g. Fishing)
+#datA_ethnog[which(datA_ethnog$Anti.title=="Fishing"),]
 
-datA <- head(recGrab)
+datA_records <- head(recGrab)
 
-tester <- sqldf("SELECT * FROM datA LEFT JOIN a ON datA.lnamID==a.LnamID")
+tester <- sqldf("SELECT * FROM datA_records LEFT JOIN datA_ethnog ON datA_records.lnamID==datA_ethnog.LnamID")
 # tester is long data.  6 records -> 22 rows now due to multiple uses per taxon.
 # Could be widened to avoid replication of lat/lon points? - think about this!
+# ZOMG: When you run this sqldf join on recGrab instead, the object is HUGE!!!!
+#str(sqldf("SELECT * FROM recGrab LEFT JOIN datA_ethnog ON recGrab.lnamID==datA_ethnog.LnamID"))
+# 'data.frame':        159922 obs. of  34 variables:
+
 
 # POTENTIAL PROBLEM: 
 # Need to re-check how ethnographic data was applied to taxon names & ensure it's CORRECT.  
 # Was anything missed? Why? Was this because it wasn't in the ethnoflora and therefore no data exists?
 # Was it due to a query error?
+
+
+### output ethnog annotations linked to Latin Names
+# needs to look like: 
+        # familyName - accepted family name (from Padme taxonomy)
+        # acceptDetAs - accepted latin name from current determination (from Padme taxonomy)
+        # acceptDetNoAuth - accepted latin name from current determination with no authority string
+        # genusName - accepted name genus only
+        # detAs - what it's currently determined as in Padme (may be an old name or synonym)
+        # USES - high level use category (Anti.title should suffice?; do not include 'Use?')
+
+
+tester_output <- sqldf("SELECT familyName, acceptDetAs, acceptDetNoAuth, genusName, detAs, Anti_title FROM tester")
+
+# only unique name + uses (but not the summary category 'Use?')
+# NOTE: syntax of names from annotations has changed from Anti.title -> Anti_title
+# so beware of this!
+tester_output <- tester_output[-which(tester_output$Anti_title=="Use?"),]
+tester_output <- unique(tester_output)
+
+#head(tester_output)
+# familyName                 acceptDetAs       acceptDetNoAuth  genusName                       detAs
+# 1   Leguminosae Indigofera articulata Gouan Indigofera articulata Indigofera Indigofera articulata Gouan
+# 4   Leguminosae Indigofera articulata Gouan Indigofera articulata Indigofera Indigofera articulata Gouan
+# 5   Leguminosae Indigofera articulata Gouan Indigofera articulata Indigofera Indigofera articulata Gouan
+# 8  Boraginaceae       Cordia obtusa Balf.f.         Cordia obtusa     Cordia       Cordia obtusa Balf.f.
+# 12 Boraginaceae       Cordia obtusa Balf.f.         Cordia obtusa     Cordia       Cordia obtusa Balf.f.
+# 14 Boraginaceae       Cordia obtusa Balf.f.         Cordia obtusa     Cordia       Cordia obtusa Balf.f.
+# Anti_title
+# 1  Animal Food- Specific Livestock
+# 4                    Food (Animal)
+# 5   Important on a Specific Island
+# 8  Animal Food- Specific Livestock
+# 12    Animal/ Livestock Management
+# 14                    Construction
+
+### NOW TRY WITH ALL DATA!!!!
+# BUT CONSIDER SIZE OF DATA TABLE!
+# THINK ABOUT DPLYR USAGE!
+# .... "'data.frame': 159922 obs. of  34 variables:"
+
+# Problems: 
+# there'll be lots of <NA> uses by the looks; this is due to accepted names 
+# being used - if updated name is applied, it may not have been scored if not in
+# the ethnoflora. Also if record contains family or genus level dets only, these 
+# won't have been scored.  Need to pull these out and fix those problems by hand!
+        # possible semi-fix/workaround might be to pull the uses of the 
+        # 'detAs' (original det) field instead of 'acceptDetAs' (accepted name) 
+        # IF (conditional code required!) Anti_title shows <NA>?
+ 
+
+
+# VERY IMPORTANT!
+# CLOSE DATABASE CONNECTIONs & REMOVE OBJECTS FROM WORKSPACE!
+odbcCloseAll()
+#rm(list=ls())
