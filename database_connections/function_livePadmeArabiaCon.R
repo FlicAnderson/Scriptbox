6ccc0e72e7ff22a0c03e25f21bfa21f4ec795662
FlicAnderson <contact@felicityanderson.co.uk>
2015-08-18 13:36:33 +0200

    added some really basic error handling in case connection does not work, previously it just said 'connection esablished' or sth even if it wasn't

--- database_connections/function_livePadmeArabiaCon.R
+++ database_connections/function_livePadmeArabiaCon.R
@@ -19,18 +19,29 @@ if (!require(RODBC)){
   library(RODBC)
 } 
 
-#define function
 livePadmeArabiaCon <- function(){
+  
   # set up connection, needs to point to padmeCODE.mdb, otherwise the imported tables don't show up!
   # locat = location of padme data file
   locat_livePadmeArabia <<- "C:/Padme/padmecode.mdb"
+  
   # open connection called "con" to file at known location
   # if this is already open it doesn't do any harm if this command is repeated I think
-    # "<<-" operator below is used to globally assign the connection so the 'con_' object can be 
-    # called outside the local environment i.e. by other scripts  
+  # "<<-" operator below is used to globally assign the connection so the 'con_' object can be 
+  # called outside the local environment i.e. by other scripts  
   con_livePadmeArabia <<- odbcConnectAccess(locat_livePadmeArabia)
-  # check the connection is working 
-  odbcGetInfo(con_livePadmeArabia)
-  # return confirmation it's working:
-  print(paste("... source database connection online: ", locat_livePadmeArabia))
+  
+  # use this for details of connection:
+  #odbcGetInfo(con_livePadmeArabia)
+  
+  # informative error if connection not created
+  if(!exists("con_livePadmeArabia")) stop("... ERROR: connection not created")
+  # return confirmation if connection works
+  if(exists("con_livePadmeArabia")) print(paste("... source database connection online: ", locat_livePadmeArabia))
+  
+  # NOTE: If this error returned when sourced: 
+    # "Error in odbcConnectAccess(locat_livePadmeArabia): 
+    # odbcConnectAccess is only usable with 32-bit Windows"
+  # then you need to use 32-bit R. In RStudio, check & change this under global options!
+  
 }
