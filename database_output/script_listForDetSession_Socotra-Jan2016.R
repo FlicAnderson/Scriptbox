fcca0244bdfb13871107cf03c1445c0a3ca89247
FlicAnderson <contact@felicityanderson.co.uk>
2016-01-20 15:42:58 +0000

    added function_getWorkingNames() and also a collNumKeeper() function to allow easier sorting of the collection numbers, splitting Millers & non-Millers apart and writing out

--- database_output/script_listForDetSession_Socotra-Jan2016.R
+++ database_output/script_listForDetSession_Socotra-Jan2016.R
@@ -37,13 +37,18 @@
 # ones at E (or no-herbarium-code, since that's probably a lot)
 # 
 
+# add working names to assist ID potentially
+source("O://CMEP\ Projects/Scriptbox/general_utilities/function_getWorkingNames.R")
+getWorkingNames()
+# herbSpxReqDet 657 obs x 36 var
+
 str(herbSpxReqDet)
 herbSpxReqDet <- tbl_df(herbSpxReqDet)
 herbSpxReqDet
 
 table(herbSpxReqDet$herbariumCode, useNA="ifany")
 # BM    E       HNT     K       UPS     <NA> 
-#  5    356     8       1       36      257
+#  5    353     8       1       36      254
 # we only can determine specimens at E, really
 
 herbSpxReqDetE <- 
@@ -67,8 +72,8 @@ sum(is.na(herbSpxReqDetE$AnyLon))
 which(is.na(herbSpxReqDetE$AnyLat))
 which(is.na(herbSpxReqDetE$AnyLon))
 
-#herbSpxReqDetE[c(585),]
-#glimpse(herbSpxReqDetE[c(585),])
+#herbSpxReqDetE[c(580),]
+#glimpse(herbSpxReqDetE[c(580),])
 
 # record H-820, Miller 8719 is actually a SPIRIT ONLY collection!  Not a herbarium specimen.
 # this info has been added to the Flic notes columns
@@ -77,14 +82,58 @@ which(is.na(herbSpxReqDetE$AnyLon))
 herbSpxReqDetE <- 
         herbSpxReqDetE %>%
         filter(!is.na(AnyLat)|!is.na(AnyLon))
-# 612 obs x 35 var
+# 606 obs x 36 var
 
 herbSpxReqDetE
 # this is the list of things that need further ID. 
 # need to remove the duplicates from it somehow though!
 
+collNumKeeper <- function(x){
+        gsub("[^0-9]", "", x)
+}
+
+herbSpxReqDetE$collNum <- as.numeric(collNumKeeper(herbSpxReqDetE$collNumFull))
+
 # pull out ones which have NO det at all to start with
 a <- herbSpxReqDet %>%
         filter(is.na(taxRank))
+# doesn't assist much, but having added the working names, shows these are incredibly vague
+        # remove the temp object now
+        #rm(a)
+
+# prepare records for output
+
+# miller/unmounted list
+# numerically
+# pull out the Miller records
+MillerSpx <- herbSpxReqDetE[which(grepl("*Miller*", herbSpxReqDetE$collector)==TRUE),]
+# MillerSpx 336 obs x 37 var
+
+# sort these numerically
+MillerSpx <- tbl_df(MillerSpx)
+MillerSpx <- 
+        MillerSpx %>%
+                arrange(collNum) %>%
+                select(recID, collector, collNumFull, herbariumCode, taxRank, familyName, acceptDetAs, detAs, workingName, dateDD, dateMM, dateYY, FlicFound, FlicStatus, FlicNotes, FlicIssue, fullLocation) 
+
+# write out MillerSpx
+# write >>>MillerSpx<<<to .csv file  
+# UNCOMMENT THESE TWO LINES TO WRITE OUT!
+message(paste0(" ... saving Miller species-level-det-requiring herbarium records to: O://CMEP\ Projects/Socotra/DeterminationRequired_Miller_herbariumSpecimens-Socotra_", Sys.Date(), ".csv"))
+write.csv(MillerSpx, file=paste0("O://CMEP\ Projects/Socotra/DeterminationRequired_Miller_herbariumSpecimens-Socotra_", Sys.Date(), ".csv"), na="", row.names=FALSE)
 
 
+# non-miller/mounted list
+# by family
+NonMillerSpx <- herbSpxReqDetE[which(grepl("*Miller*", herbSpxReqDetE$collector)==FALSE),]
+# NonMillerSpx 270 obs x 36 var
+NonMillerSpx <- tbl_df(NonMillerSpx)
+NonMillerSpx <- 
+        NonMillerSpx %>%
+        arrange(familyName, detAs) %>%
+        select(recID, collector, collNumFull, herbariumCode, taxRank, familyName, acceptDetAs, detAs, workingName, dateDD, dateMM, dateYY, FlicFound, FlicStatus, FlicNotes, FlicIssue, fullLocation) 
+
+# write >>>NonMillerSpx<<<to .csv file  
+# UNCOMMENT THESE TWO LINES TO WRITE OUT!
+message(paste0(" ... saving Non-Miller species-level-det-requiring herbarium records to: O://CMEP\ Projects/Socotra/DeterminationRequired_Miller_herbariumSpecimens-Socotra_", Sys.Date(), ".csv"))
+write.csv(NonMillerSpx, file=paste0("O://CMEP\ Projects/Socotra/DeterminationRequired_Non-Miller_herbariumSpecimens-Socotra_", Sys.Date(), ".csv"), na="", row.names=FALSE)
