d4e0ffb358db34abda2c643ec4727f1141261b05
FlicAnderson <contact@felicityanderson.co.uk>
2016-02-04 13:48:05 +0000

    changed the test database this points to & added if(NOTexists) STOP comments and error message

--- database_connections/function_TESTPadmeArabiaCon.R
+++ database_connections/function_TESTPadmeArabiaCon.R
@@ -5,9 +5,10 @@
 #
 # to call: TESTPadmeArabiaCon()
 # objects created: locat_TESTPadmeArabia; con_TESTPadmeArabia (locally global)
-# saved at: O://CMEP\-Projects/Scriptbox/function_TESTPadmeArabiaCon.R
+# saved at: O://CMEP\-Projects/Scriptbox/database_connections/function_TESTPadmeArabiaCon.R
 #
-# FUNCTION AIM: Set up and open connection to TESTAREA test-database, return confirmation.
+# FUNCTION AIM: Set up and open connection to a TEST copy of Padme Arabia 
+# ............. database, return confirmation.
 #
 # --------------------------------------------------------
 
@@ -21,16 +22,23 @@ if (!require(RODBC)){
 
 #define function
 TESTPadmeArabiaCon <- function(){
-  # set up connection, needs to point to padmeCODE.mdb, otherwise the imported tables don't show up!
-  # locat = location of padme data file
-  locat_TESTPadmeArabia <<- "Z:/socotra/TESTAREA_Databasin/padmecode.mdb"
-  # open connection called "con" to file at known location
-  # if this is already open it doesn't do any harm if this command is repeated I think
-    # "<<-" operator below is used to globally assign the connection so the 'con_' object can be 
-    # called outside the local environment i.e. by other scripts  
-  con_TESTPadmeArabia <<- odbcConnectAccess(locat_TESTPadmeArabia)
-  # check the connection is working 
-  odbcGetInfo(con_TESTPadmeArabia)
-  # return confirmation it's working:
-  print(paste("... source database connection online: ", locat_TESTPadmeArabia))
+        
+        # set up connection, needs to point to padmeCODE.mdb, otherwise imported tables don't show up!
+        # locat = location of padme data file
+        locat_TESTPadmeArabia <<- "C://Padme/TESTING/padmecode.mdb"
+        
+        # open connection called "con" to file at known location
+        # if this is already open it doesn't do any harm if this command is repeated I think
+        # "<<-" operator below is used to globally assign the connection so the 'con_' object can be 
+        # called outside the local environment i.e. by other scripts  
+        con_TESTPadmeArabia <<- odbcConnectAccess(locat_TESTPadmeArabia)
+        
+        # check the connection is working 
+        #odbcGetInfo(con_TESTPadmeArabia)
+        
+        # informative error if connection not created
+        if(!exists("con_TESTPadmeArabia")) stop("... ERROR: test-connection not created")
+        # return confirmation if connection works
+        if(exists("con_TESTPadmeArabia")) print(paste("... TEST-database connection online: ", locat_TESTPadmeArabia))
+  
 }
