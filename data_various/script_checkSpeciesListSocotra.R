bcfcf2ec3a4f793814c5848e9d70cf9435b65dd5
FlicAnderson <contact@felicityanderson.co.uk>
2016-02-15 14:59:43 +0000

    added a couple of taxa to test, called the padmeNameMatch() function

--- data_various/script_checkSpeciesListSocotra.R
+++ data_various/script_checkSpeciesListSocotra.R
@@ -71,14 +71,23 @@ latinNamesMatcher(fileLocat, fileName, rowIndex=1:800, colIndexSp=5, colIndexSsp
 
 # 2)
 
+# test taxa
+test1 <- "Peperomia blanda  (Jacq.) Kunth"
+test2 <- "Peperomia tetraphylla  Hook. & Arn."
+test3 <- "Eulophia petersii (Rchb.f.) Rchb.f."
+test4 <- "Cyanixia socotrana (Balf.f.) Goldblatt & J.C.Manning"
+
+
+
 # need to integrate the fuzzy names matcher script to suggest matches to make edits
 # and corrections easier - maybe do this using dplyr and then mutate(function) new column
 
 # source that function
 source("O://CMEP\ Projects/Scriptbox/general_utilities/function_padmeNameMatch.R")
 # to call: padmeNameMatch(checkMe=NULL, taxonType="species", authorityPresent=FALSE, taxonSingle=TRUE)
-
 # need to edit it to deal with multiple taxa (create method for taxonSingle=FALSE)
+
+padmeNameMatch(checkMe=test1, taxonType="species", authorityPresent=FALSE, taxonSingle=TRUE)
 
 
 # dplyr everything necessary
