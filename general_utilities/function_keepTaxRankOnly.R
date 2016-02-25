9cd9bb866aa559569e3a41f743b788215f51626f
FlicAnderson <contact@felicityanderson.co.uk>
2016-02-25 17:09:04 +0000

    pruning out taxonomic ranks (above species-level) to remove them from analysis dataset; planning to develop arguments to select taxonomic levels to keep, uncertain though. Currently keeps sp, ssp, var, and Sp. Nov.

---
+++ general_utilities/function_keepTaxRankOnly.R
@@ -0,0 +1,86 @@
+## FUNCTION :: function_keepTaxRankOnly.R
+# ==============================================================================
+# 25 February 2016
+# Author: Flic Anderson
+#
+# to call: keepTaxRankOnly()
+# objects created: recGrab(altered)
+# saved at: O://CMEP\-Projects/Scriptbox/general_utilities/function_keepTaxRankOnly.R
+# dependent on: "O://CMEP\ Projects/Scriptbox/database_connections/function_livePadmeArabiaCon.R"
+# dependent on: "O://CMEP\ Projects/Scriptbox/database_output/script_dataGrabFullLatLonOrGazLatLon_Socotra.R"
+#
+# AIM:  Function which pulls out herbarium records which need determinations to 
+# ....  species level as herbSpxReqDet object adapted from recGrab object
+# ....  previously created/adapted from function_getFamilies.R &   
+# ....  function_getRanks.R & running on data from recGrab output of 
+# ....  script_dataGrabFullLatLonOrGazLatLon_Socotra.R 
+#
+#
+# ---------------------------------------------------------------------------- #
+
+# CODE SUMMARY # 
+
+# 0) 
+# 1)  
+# 2) 
+# 3) 
+# 4) 
+# 5) 
+
+# ---------------------------------------------------------------------------- #
+
+
+# 0)
+
+keepTaxRankOnly <- function(){
+        
+        # does recGrab object exist?  
+        # informative error if recGrab doesn't exist
+        if(!exists("recGrab")) stop("... ERROR: recGrab object doesn't exist")
+        
+        # create tbl_df obj
+        recGrab <- tbl_df(recGrab)
+        
+        #table(recGrab$taxRank, useNA="ifany")
+        # Class    Division      Family       forma       Genus 
+        # 1          12         273           4        4136 
+        # Sp.    Sp. Nov.     species sub complex   Subfamily 
+        # 4          20       25588           3           2 
+        # subspecies    variety     <NA> 
+        #  431          422         288 
+        
+        # pull out records which will be excluded
+        excludedRex <- 
+                recGrab %>%
+                filter(
+                        taxRank=="Class"|
+                                taxRank=="Division"|
+                                taxRank=="Family"|
+                                taxRank=="forma"|
+                                taxRank=="Genus"|
+                                taxRank=="Sp."|
+                                #taxRank=="Sp. Nov."|
+                                taxRank=="Subfamily"|
+                                taxRank=="sub complex"|
+                                is.na(taxRank)
+                ) %>%
+                arrange(collector, collNumFull) 
+        # 7423 records excluded 2016-02-25
+        
+        # amend recGrab object to contain only records with dets at species-level or below
+        recGrab <<- 
+                recGrab %>%
+                filter(
+                        taxRank=="species"|
+                        taxRank=="subspecies"|
+                        taxRank=="variety"|
+                        taxRank=="Sp. Nov."
+                ) %>%
+                arrange(acceptDetAs, collector, collNumFull)
+        # 26441 records remain for analyses
+
+        # output messages
+        message(paste0("... ", nrow(recGrab), " records OK for analyses (sp-level or below)  :D"))
+        message(paste0("... ", nrow(excludedRex), " records removed (above sp-level determinations)  :c"))
+        
+}
