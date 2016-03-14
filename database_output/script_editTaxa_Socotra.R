4fc9978abae76cb7cb6dcce9f357cdf5c53b9055
FlicAnderson <contact@felicityanderson.co.uk>
2016-03-14 17:56:53 +0000

    completed the method for removing the lichens and chara, added total number of accepted taxa to the writeout message

--- database_output/script_editTaxa_Socotra.R
+++ database_output/script_editTaxa_Socotra.R
@@ -59,30 +59,68 @@ taxaListForChecks <-
 
 # Ignore the lichens!
 # genusName==
-lichens <- c("Amphiloma", "Arthonia", "Batarrea", "Bathelium", "Blastenia", "Buellia", "Callopisma", "Campylopus", "Chiodecton", "Collema", "Corticium", "Dacrymyces", "Eutypa", "Fabronia", "Graphina", "Graphis", "Lecanora", "Lecidea", "Lentinus", "Microglaena", "Normandina", "Opegrapha", "Ostropa", "Parmelia", "Pertusaria", "Philonotis", "Physcia", "Placodium", "Podaxon", "Polyporus", "Pyxine", "Ramalina", "Rinodina", "Roccella", "Schlotheimia", "Sphinctrina", "Stereum", "Sticta", "Stictina", "Symblepharis", "Synechoblastus", "Theloschistes", "Tortula", "Trametes", "Urceolaria", "Usnea", "Valsa", "Weisia")
+#lichens <- c("Amphiloma", "Arthonia", "Batarrea", "Bathelium", "Blastenia", "Buellia", "Callopisma", "Campylopus", "Chiodecton", "Collema", "Corticium", "Dacrymyces", "Eutypa", "Fabronia", "Graphina", "Graphis", "Lecanora", "Lecidea", "Lentinus", "Microglaena", "Normandina", "Opegrapha", "Ostropa", "Parmelia", "Pertusaria", "Philonotis", "Physcia", "Placodium", "Podaxon", "Polyporus", "Pyxine", "Ramalina", "Rinodina", "Roccella", "Schlotheimia", "Sphinctrina", "Stereum", "Sticta", "Stictina", "Symblepharis", "Synechoblastus", "Theloschistes", "Tortula", "Trametes", "Urceolaria", "Usnea", "Valsa", "Weisia")
+
 # Ignore Chara
 # genusName==
-chara <- c("Chara")
+#chara <- c("Chara")
+
+
 
 # remove lichens and chara
-#recGrab <- 
+recGrab <- 
         recGrab %>%
-        filter(genusName !=lichens & genusName !=chara)
-        
-        i <- 1
-        for(i in 1:length(lichens)){
-        print(cat(paste0(" genusName != lichens[", i , "] |"), sep=""))
-        i <- i + 1
-        }
-        
-#         i <- 1
-#         for(i in lichens){
-#                 a <- lichens[i]
-#                 paste0("genusName != lichens[", a , "] |")
-#                 i <- i + 1
-#         }
-        
-        
+                filter(genusName != "Amphiloma") %>%
+                filter(genusName != "Arthonia") %>%
+                filter(genusName != "Batarrea") %>%
+                filter(genusName != "Bathelium") %>%
+                filter(genusName != "Blastenia") %>%
+                filter(genusName != "Buellia") %>%
+                filter(genusName != "Callopisma") %>%
+                filter(genusName != "Campylopus") %>%
+                filter(genusName != "Chiodecton") %>%
+                filter(genusName != "Collema") %>%
+                filter(genusName != "Corticium") %>%
+                filter(genusName != "Dacrymyces") %>%
+                filter(genusName != "Eutypa") %>%
+                filter(genusName != "Fabronia") %>%
+                filter(genusName != "Graphina") %>%
+                filter(genusName != "Graphis") %>%
+                filter(genusName != "Lecanora") %>%
+                filter(genusName != "Lecidea") %>%
+                filter(genusName != "Lentinus") %>%
+                filter(genusName != "Microglaena") %>%
+                filter(genusName != "Normandina") %>%
+                filter(genusName != "Opegrapha") %>%
+                filter(genusName != "Ostropa") %>%
+                filter(genusName != "Parmelia") %>%
+                filter(genusName != "Pertusaria") %>%
+                filter(genusName != "Philonotis") %>%
+                filter(genusName != "Physcia") %>%
+                filter(genusName != "Placodium") %>%
+                filter(genusName != "Podaxon") %>%
+                filter(genusName != "Polyporus") %>%
+                filter(genusName != "Pyxine") %>%
+                filter(genusName != "Ramalina") %>%
+                filter(genusName != "Rinodina") %>%
+                filter(genusName != "Roccella") %>%
+                filter(genusName != "Schlotheimia") %>%
+                filter(genusName != "Sphinctrina") %>%
+                filter(genusName != "Stereum") %>%
+                filter(genusName != "Sticta") %>%
+                filter(genusName != "Stictina") %>%
+                filter(genusName != "Symblepharis") %>%
+                filter(genusName != "Synechoblastus") %>%
+                filter(genusName != "Theloschistes") %>%
+                filter(genusName != "Tortula") %>%
+                filter(genusName != "Trametes") %>%
+                filter(genusName != "Urceolaria") %>%
+                filter(genusName != "Usnea") %>%
+                filter(genusName != "Valsa") %>%
+                filter(genusName != "Weisia") %>%
+                filter(genusName != "Chara")
+
+
 # substitutions
 # using acceptDetAs==
 source("O://CMEP\ Projects/Scriptbox/database_output/script_editTaxa_Socotra_replacementInfo.R")
@@ -103,7 +141,7 @@ recGrab <<- recGrab
 
 # Number of taxa:
 length(unique(recGrab$acceptDetAs))
-# 971
+# 870 at 2016-03-14
 
 # create object
 taxaListSocotra <- unique(recGrab$acceptDetAs)
@@ -119,7 +157,7 @@ taxaListForChecks <-
         arrange(familyName, genusName, acceptDetAs)
 
 # write list of unique taxa
-message(paste0(" ... saving revised list of accepted taxa names in analysis set to: O://CMEP\ Projects/Socotra/analysisTaxaListSocotra_RevisedChecklist_", Sys.Date(), ".csv"))
+message(paste0("... saving revised list of ", length(taxaListSocotra), " accepted taxa names in analysis set to: O://CMEP\ Projects/Socotra/analysisTaxaListSocotra_RevisedChecklist_", Sys.Date(), ".csv"))
 write.csv(taxaListForChecks, file=paste0("O://CMEP\ Projects/Socotra/analysisTaxaListSocotra_RevisedChecklist_", Sys.Date(), ".csv"), row.names=FALSE)
 
 # tidy up
