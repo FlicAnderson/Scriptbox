7145876ed4d8b513ecd92133156deaa606e6db7a
FlicAnderson <contact@felicityanderson.co.uk>
2016-05-03 18:27:11 +0100

    added same commented bits from script_ethnographicAnnotationsPadme.R to show same names fixes required, only need to copy these into O://CMEP etc and properly attach fixes probably similar to editTaxa_Socotra etc to get them working. Consider output though. Perhaps easiest to just re-writeout the spreadsheet to csv for Marine

--- database_importing/script_endemicAnnotationsPadme.R
+++ database_importing/script_endemicAnnotationsPadme.R
@@ -77,3 +77,60 @@ endemics %>%
 ##### TO DO: 
 # link to latin names etc etc & write endemic y/n data into the database.  
 #### 
+
+# # Padme Data:: ethnographicAnnotationsPadme.R
+# # ======================================================== 
+# # 03 May 2016
+# # Author: Flic Anderson
+# # dependent on: "O:/CMEP Projects/Scriptbox/database_importing/script_latinNamesMatcher.R"; 
+# # ... 
+# 
+# 
+# # AIM: to write ethnographic annotation scores (by Anna Hunt from "Ethnoflora of
+# # ... Socotra") to categories already input into Padmeand match the names in her
+# # ... spreadsheet to Padme latin names to allow linking of annotation scores to 
+# # ... Padme's tables by associating matching latin names with their Padme ID, 
+# # ... matching the column headings to annotation titles and annotation list 
+# # ... members and write in those scores to the annotation list selection table
+# # ... and other tables as necessary.
+# 
+# # list of functions defined: importEthnog_xlsx(readEthnogInfo; 
+# 
+# rm(list=ls())
+# 
+# # source functions:
+# # test location
+# #source("O:/CMEP\ Projects/Scriptbox/function_TESTPadmeArabiaCon.R")
+# source("O:/CMEP\ Projects/Scriptbox/database_connections/function_livePadmeArabiaCon.R")
+# source("O:/CMEP\ Projects/Scriptbox/database_importing/script_latinNamesMatcher.R")
+# 
+# # running script_latinNamesMatcher, selected "Z://fufluns//databasin//taxaDataGrab//Socotra SPECIES LIST.xlsx": 
+# # ........ please choose file to check names from: 
+# # [1] "... using spreadsheet method to import file..."
+# # [1] "... source database connection online:  C:/Padme/padmecode.mdb"
+# # ........ enter 'TRUE' if taxon names HAVE authorities 
+# # attached (ie. in same column), or 'FALSE' if there is NO authority information attached
+# # ... FALSE
+# # ........ enter FIRST ROW index to read in from - format '1' 
+# # ... 5
+# # ........ enter LAST ROW index to read in - format '295' (if uncertain as to exact number, overestimate!) 
+# # ... 921
+# # ........ enter COLUMN index to read in - format '1,2' 
+# # - 1st column species names, 2nd column for any subspecific epithets 
+# # (example: column 1 contains 'Adenium obesum', column 2 contains 'subsp. sokotranum')
+# # ... 4,6
+# # [1] "... all names in correct format; carry on with analysis"
+# # [1] "... using spreadsheet method to extract and check names... "
+# # [1] "... 15 names need to be fixed from determinations << Z:\\fufluns\\databasin\\taxaDataGrab\\Socotra SPECIES LIST.xlsx"
+# # ........ choose or create a .CSV file to hold the names requiring checking/fixing
+# # [1] "... 15 names requiring manual checking/fixing saved to file >> C:\\Users\\fanderson\\Desktop\\ethnogoutput.csv"
+# # [1] "... name checking complete!"
+# 
+# # found CORRECT names for taxa, added to column called replaceWith in C:\\Users\\fanderson\\Desktop\\ethnogoutput.csv
+# 
+# # read in corrected names: 
+# datA <- read.csv("C:\\Users\\fanderson\\Desktop\\ethnogoutput.csv")
+# #names(datA)
+# #[1] "Species_Name"        "Subspecific_Epithet" "Taxon"               "replaceWith" 
+# 
+# # datA$Taxon + datA$replaceWith should provide the data pairs for updating the names
